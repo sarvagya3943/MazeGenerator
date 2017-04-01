c995e70c5971744c0fafc32ac1d9db6ca2336a4f
sarvagya3943 <sarvagyaagarwal01@gmail.com>
2017-04-02 05:05:46 +0530

    Add comments

--- src/Cell.java
+++ src/Cell.java
@@ -4,14 +4,16 @@ import javax.sql.RowSet;
 
 
 class Cell {
-
+	// Coordinates of the top-left corner of the grid cell 
 	private int r , c ;
+	// Store which all cells are present in the grid  
 	private boolean[] walls = new boolean[4] ; 
 	public boolean visited ; 
 	Cell(int r,int c )
 	{
 		this.r = r ; 
 		this.c = c ; 		
+		// initially which all walls are present 
 		for(int i = 0 ; i < 4 ; ++i ) {
 			walls[i] = true ; 
 		}
@@ -20,9 +22,11 @@ class Cell {
 	
 	public int getr() {return r ;}
 	public int getc() {return c ;}
+	// return true if the wall is present 
 	public boolean isWall(int index) {
 		return walls[index] ; 
 	}
+	// set the wall's visibility 
 	public void setWall(int index,boolean f) {
 		walls[index] = f ; 
 	}
